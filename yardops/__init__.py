bcdd8fc4ef3ad6beffe5d3480be8709e993ac0c7
Rama Shesha Sai Satuluri <rama.shesha@MacBook-Pro-7.local>
2026-03-17 17:16:55 +0530

    some more fixes

--- yardops/__init__.py
+++ yardops/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from .extensions import db
-from config import Config
+from .config import Config   
+
+from flask_migrate import Migrate  
+from yardops.models import *  
+
+migrate = Migrate()  
+
 
 def create_app():
     app = Flask(__name__)
@@ -8,9 +14,11 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)   
+
 
     @app.route("/")
     def home():
-        return "YardOps Backend Running "
+        return "YardOps Backend Running"
 
     return app
